96e45c62ce4998cad167a84ccd53a09aae795a2f
Maximilian Schmidt <landlaeufer@gmail.com>
2021-12-24 15:21:06 +0100

    wip

--- proto-stubs/message.pyi
+++ proto-stubs/message.pyi
@@ -1,59 +1,80 @@
 import typing as t
 
 from google.protobuf import descriptor_pb2, message
-
 from proto.fields import Field
+
 from proto.marshal import Marshal
 
 MessageType = t.TypeVar('MessageType', bound='Message')
 
+
 class MessageMeta(type):
     def __new__(mcls, name, bases, attrs): ...
+
     @classmethod
     def __prepare__(mcls, name, bases, **kwargs): ...
+
     @property
     def meta(cls): ...
 
     @t.overload
     def pb(cls, obj: None = ..., *, coerce: bool = ...) -> t.Type[message.Message]: ...
+
     @t.overload
     def pb(cls, obj: t.Any = ..., *, coerce: bool = ...) -> message.Message: ...
+
     def pb(cls, obj: t.Any | None = ..., *, coerce: bool = ...) -> message.Message: ...
+
     def wrap(cls, pb): ...
+
     def serialize(cls, instance) -> bytes: ...
+
     def deserialize(cls: t.Type[MessageType], payload: bytes) -> MessageType: ...
+
     def to_json(
-        cls,
-        instance,
-        *,
-        use_integers_for_enums: bool = ...,
-        including_default_value_fields: bool = ...,
-        preserving_proto_field_name: bool = ...
+            cls,
+            instance,
+            *,
+            use_integers_for_enums: bool = ...,
+            including_default_value_fields: bool = ...,
+            preserving_proto_field_name: bool = ...
     ) -> str: ...
+
     def from_json(cls: t.Type[MessageType],
                   payload,
                   *,
                   ignore_unknown_fields: bool = ...) -> MessageType: ...
+
     def to_dict(
-        cls: t.Type[MessageType],
-        instance,
-        *,
-        use_integers_for_enums: bool = ...,
-        preserving_proto_field_name: bool = ...
-    ) -> MessageType: ...
+            cls: t.Type[MessageType],
+            instance,
+            *,
+            use_integers_for_enums: bool = ...,
+            preserving_proto_field_name: bool = ...
+    ) -> t.Dict[str, t.Any]: ...
+
     def copy_from(cls, instance, other) -> None: ...
+
 
 class Message(metaclass=MessageMeta):
     def __init__(
-        self, mapping: t.Any | None = ..., *, ignore_unknown_fields: bool = ..., **kwargs
+            self, mapping: t.Any | None = ..., *, ignore_unknown_fields: bool = ..., **kwargs
     ) -> None: ...
+
     def __bool__(self): ...
+
     def __contains__(self, key): ...
+
     def __delattr__(self, key) -> None: ...
+
     def __eq__(self, other): ...
+
     def __getattr__(self, key): ...
+
     def __ne__(self, other): ...
+
     def __setattr__(self, key, value): ...
+
 
 class _MessageInfo:
     package: t.Any
@@ -62,13 +83,15 @@ class _MessageInfo:
     fields: t.Any
     fields_by_number: t.Any
     marshal: t.Any
+
     def __init__(
-        self,
-        fields: t.List[Field],
-        package: str,
-        full_name: str,
-        marshal: Marshal,
-        options: descriptor_pb2.MessageOptions,
+            self,
+            fields: t.List[Field],
+            package: str,
+            full_name: str,
+            marshal: Marshal,
+            options: descriptor_pb2.MessageOptions,
     ) -> None: ...
+
     @property
     def pb(self) -> t.Type[message.Message]: ...
