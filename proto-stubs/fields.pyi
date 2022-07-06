ab0e9be43dda85b4b0aa512b44ac9b8dd0fcd380
Maximilian Schmidt <landlaeufer@gmail.com>
2022-07-06 08:55:45 +0200

    Updated stubs but Pycharms Typechecker sucks so let's not overcomplicate it

--- proto-stubs/fields.pyi
+++ proto-stubs/fields.pyi
@@ -2,30 +2,30 @@ from __future__ import annotations
 import typing as t
 
 from google.protobuf import descriptor_pb2
-from proto.primitives import T_enum, T_MESSAGE
 from proto.message import Message as ProtoMessage
 
+T = t.TypeVar('T')
 
-class Field(t.Generic[T_enum]):
+class Field(t.Generic[T]):
     repeated: bool
     mcls_data: t.Any
     parent: t.Any
     number: t.Any
-    proto_type: T_enum
-    message: t.Any
-    enum: t.Any
+    proto_type: int
+    message: t.Type[T] | None
+    enum: t.Type[T] | None
     json_name: t.Any
-    optional: t.Any
-    oneof: t.Any
+    optional: bool
+    oneof: str
 
     @t.overload
     def __init__(
-            self: Field[T_enum],
-            proto_type: T_enum,
+            self: Field[t.Any],
+            proto_type: int,
             number: int,
             *,
-            message: t.Any | None = ...,
-            enum: t.Any | None = ...,
+            message: None = ...,
+            enum: None = ...,
             oneof: str = ...,
             json_name: str = ...,
             optional: bool = ...
@@ -33,12 +33,25 @@ class Field(t.Generic[T_enum]):
 
     @t.overload
     def __init__(
-            self: Field[T_MESSAGE],
-            proto_type: t.Type[ProtoMessage],
+            self: Field[T],
+            proto_type: int,
             number: int,
             *,
-            message: t.Any | None = ...,
-            enum: t.Any | None = ...,
+            message: t.Type[T] = ...,
+            enum: None = ...,
+            oneof: str = ...,
+            json_name: str = ...,
+            optional: bool = ...
+    ) -> None: ...
+
+    @t.overload
+    def __init__(
+            self: Field[T],
+            proto_type: int,
+            number: int,
+            *,
+            message: None = ...,
+            enum: t.Type[T] = ...,
             oneof: str = ...,
             json_name: str = ...,
             optional: bool = ...
@@ -56,12 +69,19 @@ class Field(t.Generic[T_enum]):
     @property
     def pb_type(self): ...
 
+    @t.overload
+    def __get__(self: Field[T], instance: object, owner: t.Type[t.Any] | None = None) -> T:
+        ...
 
-class RepeatedField(Field):
+
+class RepeatedField(Field[T]):
     repeated: bool
 
+    @t.overload
+    def __get__(self: RepeatedField[T], instance: object, owner: t.Type[t.Any] | None = None) -> t.Iterable[T]:
+        ...
 
-class MapField(Field):
+class MapField(Field[T]):
     map_key_type: t.Any
 
     def __init__(
--- proto-stubs/primitives.pyi
+++ proto-stubs/primitives.pyi
@@ -1,7 +1,6 @@
 import enum
 from typing import Literal, TypeVar
 
-
 class ProtoType(enum.IntEnum):
     DOUBLE: int
     FLOAT: int
@@ -21,24 +20,26 @@ class ProtoType(enum.IntEnum):
     SINT32: int
     SINT64: int
 
+# we can't use the values of the enum, since Pycharm doesn't support it,
+# see https://youtrack.jetbrains.com/issue/PY-35235/Support-PEP-586-Literal-Types
 
-T_DOUBLE = Literal[ProtoType.DOUBLE]
-T_FLOAT = Literal[ProtoType.FLOAT]
-T_INT64 = Literal[ProtoType.INT64]
-T_UINT64 = Literal[ProtoType.UINT64]
-T_INT32 = Literal[ProtoType.INT32]
-T_FIXED64 = Literal[ProtoType.FIXED64]
-T_FIXED32 = Literal[ProtoType.FIXED32]
-T_BOOL = Literal[ProtoType.BOOL]
-T_STRING = Literal[ProtoType.STRING]
-T_MESSAGE = Literal[ProtoType.MESSAGE]
-T_BYTES = Literal[ProtoType.BYTES]
-T_UINT32 = Literal[ProtoType.UINT32]
-T_ENUM = Literal[ProtoType.ENUM]
-T_SFIXED32 = Literal[ProtoType.SFIXED32]
-T_SFIXED64 = Literal[ProtoType.SFIXED64]
-T_SINT32 = Literal[ProtoType.SINT32]
-T_SINT64 = Literal[ProtoType.SINT64]
+T_DOUBLE = Literal[1]
+T_FLOAT = Literal[2]
+T_INT64 = Literal[3]
+T_UINT64 = Literal[4]
+T_INT32 = Literal[5]
+T_FIXED64 = Literal[6]
+T_FIXED32 = Literal[7]
+T_BOOL = Literal[8]
+T_STRING = Literal[9]
+T_MESSAGE = Literal[10]
+T_BYTES = Literal[11]
+T_UINT32 = Literal[12]
+T_ENUM = Literal[13]
+T_SFIXED32 = Literal[14]
+T_SFIXED64 = Literal[15]
+T_SINT32 = Literal[16]
+T_SINT64 = Literal[17]
 
 T_enum = TypeVar(
     'T_enum',
